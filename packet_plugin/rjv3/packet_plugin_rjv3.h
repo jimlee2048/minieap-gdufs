4e27a86517c80f9b0d1f17f244a7342f94571b21
Jim Lee <jimlee2002@outlook.com>
2021-10-31 02:03:34 +0800

    modified

--- packet_plugin/rjv3/packet_plugin_rjv3.h
+++ packet_plugin/rjv3/packet_plugin_rjv3.h
@@ -7,11 +7,11 @@
 
 #define DEFAULT_HEARTBEAT_INTERVAL  60
 #define DEFAULT_MAX_DHCP_COUNT      3
-#define DEFAULT_SERVICE_NAME        "internet"
-#define DEFAULT_VER_STR             "RG-SU For Linux V1.0"
+#define DEFAULT_SERVICE_NAME        "default"
+#define DEFAULT_VER_STR             "RG-SU For Linux V1.30"
 #define DEFAULT_DHCP_SCRIPT         ""
 #define DEFAULT_EAP_BCAST_ADDR      BROADCAST_STANDARD
-#define DEFAULT_DHCP_TYPE           DHCP_AFTER_AUTH
+#define DEFAULT_DHCP_TYPE           DHCP_DOUBLE_AUTH
 
 PACKET_PLUGIN* packet_plugin_rjv3_new();
 #endif
